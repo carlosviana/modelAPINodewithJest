4b10d092f7434405c295cc0998f2f5fffabd6bd6
carlosviana <viana.mail@gmail.com>
2019-08-20 10:21:31 -0300

    instalado swagger

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 const routes = require("express").Router();
 const authMiddleware = require("../src/app/middleware/auth");
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerJson = require("./swagger.json");
 const SessionController = require("./app/controllers/SessionController");
 
 routes.post("/sessions", SessionController.store);
+
+routes.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
+
+routes.get("/", function(req, res) {
+  res.redirect("/swagger");
+});
 
 routes.use(authMiddleware);
 
